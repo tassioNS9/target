9d5d2ea22af8e4efbd1df9674fa6241ca1b04d60
Tassio Neves <tassions@hotmail.com>
2026-08-12 20:29:32 -0300

    feat: add Summary component with styles for displaying labeled values

---
+++ src/components/Summary/styles.ts
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+import { colors } from "@/theme/colors";
+import { fontFamily } from "@/theme/fontFamily";
+
+export const styles = StyleSheet.create({
+  container: {
+    gap: 5,
+  },
+  header: {
+    flexDirection: "row",
+    gap: 5,
+    alignItems: "center",
+  },
+  label: {
+    fontSize: 10,
+    color: colors.blue[300],
+    fontFamily: fontFamily.regular,
+  },
+  value: {
+    fontSize: 18,
+    color: colors.white,
+    fontFamily: fontFamily.regular,
+  },
+});
