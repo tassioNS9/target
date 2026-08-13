79db06ba6d947f32510828ee854587b70ec82904
Tassio Neves <tassions@hotmail.com>
2026-08-13 13:01:01 -0300

    feat: enhance Index component with navigation for targets and add new meta button

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,6 +2,8 @@ import { StyleSheet, Text, View } from "react-native";
 import { HomeHeader } from "@/components/HomeHeader";
 import { Target } from "@/components/Target";
 import { List } from "@/components/List";
+import { Button } from "@/components/Button";
+import { router } from "expo-router";
 export default function Index() {
   const sumaryData = {
     input: { label: "Entradas", value: "R$ 6.148,00" },
@@ -40,10 +42,19 @@ export default function Index() {
         title="Metas"
         data={targetData}
         keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <Target data={item} />}
+        renderItem={({ item }) => (
+          <Target
+            data={item}
+            onPress={() => router.navigate(`/in-progress/${item.id}`)}
+          />
+        )}
         emptyMessage="Nenhuma meta. Toque em nova meta para criar uma."
         containerStyle={{ paddingHorizontal: 24 }}
       />
+
+      <View style={{ padding: 24, paddingBottom: 32 }}>
+        <Button title="Nova meta" onPress={() => router.navigate("/target")} />
+      </View>
     </View>
   );
 }
