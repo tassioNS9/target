a6bdfa953944853dcd325787f64c2234af5cafb7
Tassio Neves <tassions@hotmail.com>
2026-08-16 18:20:13 -0300

    feat: add Progress component to display financial progress details

--- src/app/in-progress/[id].tsx
+++ src/app/in-progress/[id].tsx
@@ -1,14 +1,25 @@
-import { View, Text, Button } from "react-native";
+import { View } from "react-native";
 import { useLocalSearchParams } from "expo-router";
-import { router } from "expo-router";
+import { PageHeader } from "@/components/PageHeader";
 
+import { Progress } from "@/components/Progress";
+
+const details = {
+  current: "R$ 1.000,00",
+  target: "R$ 1.790,00",
+  percentage: 60,
+};
 export default function InProgress() {
   const { id } = useLocalSearchParams<{ id: string }>();
 
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Progresso {id}</Text>
-      <Button title="Voltar" onPress={() => router.back()} />
+    <View style={{ flex: 1, padding: 24, gap: 32 }}>
+      <PageHeader
+        title="Apple Watch Series 9"
+        subtitle="Economize para alcançar sua meta financeira"
+        rightButton={{ icon: "edit", onPress: () => {} }}
+      />
+      <Progress data={details} />
     </View>
   );
 }
