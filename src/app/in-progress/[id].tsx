56cd2a5342585c7158304d91a7f31ce30408c7af
Tassio Neves <tassions@hotmail.com>
2026-08-17 13:37:47 -0300

    feat: add transaction list to InProgress component

--- src/app/in-progress/[id].tsx
+++ src/app/in-progress/[id].tsx
@@ -1,14 +1,33 @@
 import { View } from "react-native";
 import { useLocalSearchParams } from "expo-router";
 import { PageHeader } from "@/components/PageHeader";
-
+import { List } from "@/components/List";
+import { Transaction, TransactionProps } from "@/components/Transaction";
 import { Progress } from "@/components/Progress";
+
+import { TransactionTypes } from "@/utils/TransactionTypes";
 
 const details = {
   current: "R$ 1.000,00",
   target: "R$ 1.790,00",
   percentage: 60,
 };
+
+const transactions: TransactionProps[] = [
+  {
+    id: "1",
+    value: "R$ 300,00",
+    date: "12/04/2026",
+    description: "CDB de 110% no banco XPTO",
+    type: TransactionTypes.Input,
+  },
+  {
+    id: "2",
+    value: "R$ 20,00",
+    date: "14/04/2026",
+    type: TransactionTypes.Output,
+  },
+];
 export default function InProgress() {
   const { id } = useLocalSearchParams<{ id: string }>();
 
@@ -20,6 +39,14 @@ export default function InProgress() {
         rightButton={{ icon: "edit", onPress: () => {} }}
       />
       <Progress data={details} />
+
+      <List
+        title="Transações"
+        data={transactions}
+        renderItem={({ item }) => (
+          <Transaction data={item} onRemove={() => {}} />
+        )}
+      />
     </View>
   );
 }
