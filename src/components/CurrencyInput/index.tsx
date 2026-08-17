477a93162f7b36a2ab35f7452dede02802eead7f
Tassio Neves <tassions@hotmail.com>
2026-08-17 15:40:56 -0300

    fix: remove prefix on Input component

--- src/components/CurrencyInput/index.tsx
+++ src/components/CurrencyInput/index.tsx
@@ -14,7 +14,6 @@ export function CurrencyInput({ label, ...rest }: InputProps) {
       <Input
         style={styles.input}
         placeholderTextColor={colors.gray[400]}
-        prefix="R$ "
         delimiter="."
         separator=","
         precision={2}
