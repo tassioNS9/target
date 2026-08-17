1ff5d112d2cd9e20d4102a95652de0bb6e072a0e
Tassio Neves <tassions@hotmail.com>
2026-08-17 15:42:55 -0300

    feat: add transaction page by id

--- src/app/transaction/[id].tsx
+++ src/app/transaction/[id].tsx
@@ -1,13 +1,30 @@
-import { View, Text, Button } from "react-native";
+import { Button } from "@/components/Button";
+import { CurrencyInput } from "@/components/CurrencyInput";
+import { Input } from "@/components/Input";
+import { PageHeader } from "@/components/PageHeader";
 import { useLocalSearchParams } from "expo-router";
-import { router } from "expo-router";
+import { View } from "react-native";
+
 export default function Transaction() {
-  const { id } = useLocalSearchParams<{ id: string }>();
+  const params = useLocalSearchParams<{ id: string }>();
 
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Transaction {id}</Text>
-      <Button title="Voltar" onPress={() => router.back()} />
+    <View style={{ flex: 1, padding: 24 }}>
+      <PageHeader
+        title="Nova transação"
+        subtitle="A cada valor guardado você fica mais próximo da sua meta. Se esforce para guardar e evitar retirar."
+      />
+
+      <View style={{ marginTop: 32, gap: 24 }}>
+        <CurrencyInput label="Valor (R$)" value={0} />
+
+        <Input
+          label="Motivo (opcional)"
+          placeholder="Ex: Investir em CDB de 110% no banco XPTO"
+        />
+
+        <Button title="Salvar" />
+      </View>
     </View>
   );
 }
