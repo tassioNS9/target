dfd45249cc34bd17dbfac46c19e7fe957cf37ed8
Tassio Neves <tassions@hotmail.com>
2026-08-17 15:41:49 -0300

    fix: change label on CurrencyInput

--- src/app/target.tsx
+++ src/app/target.tsx
@@ -17,7 +17,7 @@ export default function Target() {
           placeholder="Ex: Viagem para praia, Apple Watch"
         />
 
-        <CurrencyInput label="Valor alvo" value={0} />
+        <CurrencyInput label="Valor alvo (R$)" value={0} />
         <Button title="Salvar" />
       </View>
     </View>
