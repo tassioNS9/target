c1fb74220f5496ea4ab82bb9dbae084858a4eb8d
Tassio Neves <tassions@hotmail.com>
2026-08-17 13:37:38 -0300

    feat: add Transaction component

---
+++ src/components/Transaction/styles.ts
@@ -0,0 +1,26 @@
+import { colors } from "@/theme/colors";
+import { fontFamily } from "@/theme/fontFamily";
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    height: 72,
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 7,
+  },
+  info: {
+    flex: 1,
+    gap: 7,
+  },
+  value: {
+    fontSize: 14,
+    fontFamily: fontFamily.medium,
+    color: colors.black,
+  },
+  description: {
+    fontSize: 12,
+    fontFamily: fontFamily.regular,
+    color: colors.gray[500],
+  },
+});
