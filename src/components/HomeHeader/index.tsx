4ec51e547ad39db343e9efcad07158472cdce2ac
Tassio Neves <tassions@hotmail.com>
2026-08-18 19:39:44 -0300

    fix: update Summary component to use isRight prop for alignment

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -35,7 +35,7 @@ export function HomeHeader({ data }: Props) {
         />
 
         <Summary
-          isLeft
+          isRight
           data={data.output}
           icon={{ name: "arrow-downward", color: colors.red[400] }}
         />
