af7ad45db0b020eec568ca2067eab70a3e10a32a
Tassio Neves <tassions@hotmail.com>
2026-08-20 21:21:27 -0300

    feat: add update function

--- src/database/useTargetDatabase.ts
+++ src/database/useTargetDatabase.ts
@@ -5,6 +5,10 @@ export type TargetCreate = {
   amount: number;
 };
 
+export type TargetUpdate = TargetCreate & {
+  id: number;
+};
+
 export type TargetResponse = {
   id: number;
   name: string;
@@ -62,5 +66,17 @@ export function useTargetDatabase() {
       `);
   }
 
-  return { create, listBySavedValue, getById };
+  function update(data: TargetUpdate) {
+    const statement = database.prepareSync(
+      "UPDATE targets SET name = $name, amount = $amount, updated_at = CURRENT_TIMESTAMP WHERE id = $id",
+    );
+
+    statement.executeSync({
+      $id: data.id,
+      $name: data.name,
+      $amount: data.amount,
+    });
+  }
+
+  return { create, listBySavedValue, getById, update };
 }
